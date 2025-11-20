1643e9e4e5bc532e7645103a4b7bc50d98162e47
allis <1951531337@qq.com>
2025-11-20 21:12:06 +0800

    home新建列表

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./styles/base.scss";
+import { lazyPlugin } from "@/directives";
 
 // .catch(err => {
 //   console.error('请求出错', err);
@@ -15,5 +16,5 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(lazyPlugin);
 app.mount("#app");
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,20 +1,24 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
-  timeout:5000
-})
+  baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
+  timeout: 5000,
+});
 
 //拦截器
 //请求
-http.interceptors.request.use(config => {
-  return config
-}, e => Promise.reject(e))
+http.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (e) => Promise.reject(e)
+);
 //响应
-http.interceptors.response.use(res => res.data, e => {
-  return Promise.reject(e)
-})
+http.interceptors.response.use(
+  (res) => res.data,
+  (e) => {
+    return Promise.reject(e);
+  }
+);
 
-
-
-export default http
+export default http;
