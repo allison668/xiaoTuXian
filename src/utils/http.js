2a2fbfe6e8ae93e46c7b5cac55016c16da3cac92
allis <1951531337@qq.com>
2025-11-26 23:59:19 +0800

    401响应拦截器

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { ElMessage } from "element-plus";
 import "element-plus/theme-chalk/el-message.css";
-
+import { useUserStore } from "@/stores/user";
+import router from "@/router";
 const http = axios.create({
   baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
   timeout: 5000,
@@ -11,18 +12,29 @@ const http = axios.create({
 //请求
 http.interceptors.request.use(
   (config) => {
+    const userStore = useUserStore();
+    const token = userStore.userInfo.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (e) => Promise.reject(e)
 );
 //响应
 http.interceptors.response.use(
+  // const userStore = useUserStore();
   (res) => res.data,
   (e) => {
     ElMessage({
       type: "warning",
       message: e.response.data.message,
     });
+    if (e.response.status === 401) {
+      const userStore = useUserStore();
+      userStore.clearUserInfo();
+      router.push("/login");
+    }
     return Promise.reject(e);
   }
 );
